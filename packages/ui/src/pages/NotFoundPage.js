4e4ee937aff372cac4d49ded4c4b516fb5838731
Oliver Barnwell <barnwelloliver@gmail.com>
2018-09-07 17:02:22 +0100

    fix: add component

---
+++ packages/ui/src/pages/NotFoundPage.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+import config from '../config';
+
+import PageLayout from '../components/PageLayout';
+import NearestLooMap from '../components/NearestLooMap';
+
+import headings from '../css/headings.module.css';
+import layout from '../components/css/layout.module.css';
+import controls from '../css/controls.module.css';
+
+class AboutPage extends Component {
+  renderMain() {
+    return (
+      <div>
+        <div>
+          <div className={layout.controls}>
+            {config.showBackButtons && (
+              <button
+                onClick={this.props.history.goBack}
+                className={controls.btn}
+              >
+                Back
+              </button>
+            )}
+          </div>
+        </div>
+        <h2 id="about" className={headings.regular}>
+          Not Found!
+        </h2>
+        <p>...</p>
+      </div>
+    );
+  }
+
+  renderMap() {
+    return <NearestLooMap numberNearest />;
+  }
+
+  render() {
+    return <PageLayout main={this.renderMain()} map={this.renderMap()} />;
+  }
+}
+
+export default AboutPage;
